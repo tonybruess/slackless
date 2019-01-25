9de7b65ce6431396c2ad0f5d9ac717af8f48fa88
Tony Bruess <tony@angel.co>
2019-01-25 13:17:41 -0500

    Make an ASAR backup and bypass gatekeeper

--- bin/patch.sh
+++ bin/patch.sh
@@ -1,38 +1,56 @@
 #!/usr/bin/env bash
 SLACKLESS_DIR=$(cd `dirname $(dirname $0)` && pwd)
 
-# Make sure Slack is installed.
-echo "slackless: finding Slack.app..."
 SLACK_DIR="/Applications/Slack.app/"
+CONTENTS_DIR="${SLACK_DIR}/Contents/"
+RESOURCES_DIR="${CONTENTS_DIR}/Resources/"
+
+# Make sure Slack is installed.
 if [ ! -d ${SLACK_DIR} ]; then
    echo "slackless: could not find Slack.app"
    exit -1
+else
+  SLACK_VERSION=`defaults read ${CONTENTS_DIR}/Info.plist CFBundleShortVersionString`
+  echo "slackless: found Slack version ${SLACK_VERSION}"
 fi
 
-# Find correct patch.
-echo "slackless: finding patch..."
-RESOURCES="/Applications/Slack.app/Contents/Resources/"
-ASAR="/Applications/Slack.app/Contents/Resources/app.asar"
-ASAR_MD5=`md5 -q ${ASAR}`
+ASAR="${RESOURCES_DIR}/app.asar"
+ASAR_BACKUP="${ASAR}.backup"
+ASAR_UNPACKED="${ASAR}.unpacked"
+
+# Use or create a backup
+if [ ! -f "${ASAR_BACKUP}" ]; then
+  cp "${ASAR}" "${ASAR_BACKUP}"
+else
+  cp "${ASAR_BACKUP}" "${ASAR}"
+fi
+
+ASAR_MD5=`md5 -q ${ASAR_BACKUP}`
 PATCH="${SLACKLESS_DIR}/patches/${ASAR_MD5}.patch"
+
+# Find correct patch.
 if [ ! -f "${PATCH}" ]; then
   echo "slackless: no patch file for your version of Slack.app asar (md5: ${ASAR_MD5})."
   exit -1
+else
+  echo "slackless: found patch ${PATCH}"
 fi
-echo "slackless: found path ${PATCH}"
 
-# Configure patch.
+# Configure patch. TODO: broken
 URL="https://tonybruess.github.io/slackless/slackless.js"
 if [[ "$1" != "" ]]; then
   URL="$1"
 fi
 
+# Quit Slack
+osascript -e 'quit app "Slack"'
+
 # Extract asar.
 echo "slackless: extracting asar..."
-cd "${RESOURCES}" && asar extract "${ASAR}" "${ASAR}.unpacked"
+cd "${RESOURCES}" && asar extract "${ASAR}" "${ASAR_UNPACKED}"
 
 # Patch.
-cd "${ASAR}.unpacked" && bash "${PATCH}" "${URL}"
+cd "${ASAR_UNPACKED}" && bash "${PATCH}" "${URL}"
 if [ $? -ne 0 ]; then
   echo "slackless: application of ${ASAR_MD5}.patch failed."
   exit -1
@@ -40,7 +58,19 @@ fi
 
 # Pack asar.
 echo "slackless: packing asar..."
-cd "${RESOURCES}" && asar pack "${ASAR}.unpacked" "${ASAR}"
+cd "${RESOURCES}" && asar pack "${ASAR_UNPACKED}" "${ASAR}"
+
+# Disable gatekeeper
+echo "slackless: temporarily disabling gatekeeper. please enter your password if prompted"
+sudo spctl --master-disable
+
+# Open Slack
+echo "slackless: opening Slack. please click 'Open' when prompted"
+open "${SLACK_DIR}"
+
+# Enable gatekeeper
+echo "slackless: re-enabling gatekeeper. please enter your password if prompted"
+sudo spctl --master-enable
 
 echo "slackless: done"
 exit 0
